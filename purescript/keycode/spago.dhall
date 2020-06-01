a1f24409dc16e96146efea7009a3e19d51856e3e
kyu-suke <ykaneoka7@gmail.com>
2020-06-02 03:02:57 +0900

    入力業を読んで出力的なことはできたがそういうのをしたいわけじゃない

--- purescript/keycode/spago.dhall
+++ purescript/keycode/spago.dhall
@@ -4,7 +4,14 @@ You can edit this file as you like.
 -}
 { name = "my-project"
 , dependencies =
-  [ "aff", "console", "effect", "node-readline", "psci-support", "signal" ]
+  [ "aff"
+  , "console"
+  , "effect"
+  , "node-buffer"
+  , "node-readline"
+  , "psci-support"
+  , "signal"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- purescript/keycode/src/Main.purs
+++ purescript/keycode/src/Main.purs
@@ -2,22 +2,46 @@ module Main where
 
 import Prelude
 
-import Data.Either (Either(..))
-import Data.Posix.Signal
+-- import Data.Either (Either(..))
+import Data.Options ((:=))
+-- import Data.Show (Buffer)
+-- import Data.Posix.Signal
 import Effect (Effect)
+-- import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Node.ReadLine (Interface, close, createConsoleInterface, noCompletion, question)
+import Node.ReadLine (Interface, close, createConsoleInterface, noCompletion, question, createInterface, output, setPrompt, terminal)
 import Node.Process (stdin, stdout, onSignal)
+import Node.Stream (onData, onDataString, end, cork)
+import Node.Buffer (Buffer, toString)
+import Node.Encoding  (Encoding(..))
+-- import Web.UIEvent.KeyboardEvent as KE
+-- import Web.Event.Event (EventType(..))
+-- import Web.Event.Internal.Types (Event)
+-- import Web.Event.EventTarget (addEventListener, eventListener)
+
 
 main :: Effect Unit
 main = do
-  interface <- createConsoleInterface noCompletion
-  question "What do you think of PureScript? " (callback interface) interface
-  where
-    callback :: Interface -> String -> Effect Unit
-    callback interface answer = do
-      log $ "Thank you for your valuable feedback: " <> answer
-      close interface
+  log "START"
+
+  -- READ STREAM
+  -- onData stdin (\s -> do
+  --         str <- toString Hex s
+  --         log str
+  --         )
+
+  -- onDataString stdin UTF8 (\s -> do
+  --         log s
+  --         end stdout (log "AAA")
+  --         )
+
+  log "END"
+
+  -- READ LINE
+  -- rl <- createInterface stdin $ output := stdout
+  -- question "????" (\s -> do
+  --   log s
+  --   close rl) rl
 
 
 -- stdin
@@ -32,3 +56,46 @@ stdin.on('keypress', function (chunk, key) {
   if (key && key.ctrl && key.name == 'c') process.exit();
 });
 -}
+
+
+
+-- main :: Effect Unit
+-- main = do
+--   interface <- createConsoleInterface noCompletion
+--   question "What do you think of PureScript? " (callback interface) interface
+--   where
+--     callback :: Interface -> String -> Effect Unit
+--     callback interface answer = do
+--       log $ "Thank you for your valuable feedback: " <> answer
+--       close interface
+-- 
+-- main :: forall e. Effect (console :: CONSOLE, readline :: READLINE, exception :: EXCEPTION | e) Unit
+-- main :: Effect Unit
+-- main = do
+--   interface <- createConsoleInterface noCompletion
+--   runAff_ (either (error <<< show) log) (runReaderT loop interface)
+--   where
+--     loop = do
+--       setPrompt "$ "
+--       dog <- question "What's your dog's name?\n"
+--       liftEffect <<< log $ "Can I pet " <> dog <> "?"
+--       str <- readLine
+--       case uncons str of
+--         Just {head: 'y'} -> liftEffect $ log "Thanks!"
+--         _ -> liftEffect $ log "C'mon! Be a sport about it!"
+--       loop
+
+-- test a =
+--   log "Key Pressed : "
+--   -- case runExcept (KE.eventToKeyboardEvent a) of
+--   --   Left err ->
+--   --     log "Event was not a keyboard event"
+--   --   Right ke -> do
+--   --     let co = KE.key ke
+--   --     log "Key Pressed : "
+-- 
+-- main = do
+--   -- documenttarget <- liftEffect $ window >>= document <#> DHT.htmlDocumentToEventTarget
+--   addEventListener (EventType "keydown") (eventListener test) true (documenttarget)
+
+
