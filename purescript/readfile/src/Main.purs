48fe6a2f0647e4a600f18b4058b3e8e1ebe60524
kyu-suke <ykaneoka7@gmail.com>
2020-06-05 22:14:12 +0900

    readtextfile

--- purescript/readfile/src/Main.purs
+++ purescript/readfile/src/Main.purs
@@ -4,7 +4,30 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Node.FS.Async(readTextFile)
+import Node.Process (argv)
+import Data.Array (reverse, head, length)
+import Data.Maybe (Maybe(Just, Nothing))
+import Node.Encoding(Encoding(UTF8))
+import Data.Either (Either(..), either)
 
+
+-- spago run -a hoge.txt
 main :: Effect Unit
 main = do
-  log "🍝"
+  args <- argv
+  log $ show args
+
+  let fileName = case length args of
+                   3 -> getFileName args
+                   _ -> Nothing
+
+  case fileName of
+    Just x -> readTextFile UTF8 x $ \x -> do
+                                      log "\n\nreadTextFile result:"
+                                      either (log <<< show) log x
+    Nothing -> log "errrrrr"
+
+
+getFileName :: Array String -> Maybe String
+getFileName args = head $ reverse args
