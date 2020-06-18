9cb3d19dad8122261541c395e90e6786da87072c
kyu-suke <ykaneoka7@gmail.com>
2020-06-19 03:14:53 +0900

    monado

--- purescript/terminal/src/Main.purs
+++ purescript/terminal/src/Main.purs
@@ -8,7 +8,7 @@ import Node.Stream (Readable, Writable)
 import Node.Process (stdin, stdout, argv)
 import Data.Function.Uncurried (Fn1, runFn1, Fn4, runFn4)
 import Data.Generic.Rep (class Generic)
-import Data.Array (replicate, length, head, reverse)
+import Data.Array (replicate, length, head, reverse, filter, snoc)
 import Data.Traversable (for)
 import Node.FS.Sync(readTextFile)
 import Data.Maybe (Maybe(Just, Nothing))
@@ -16,6 +16,12 @@ import Node.Encoding(Encoding(UTF8))
 import Data.Either (Either(..), either)
 import Data.String.Common (split)
 import Data.String.Pattern (Pattern(..))
+import Data.Foldable (foldl)
+
+import Control.Monad.Writer (writer)
+import Data.Tuple (Tuple)
+import Control.Monad.State
+import Control.Monad.State.Class
 
 
 foreign import getColumns :: Int
@@ -32,39 +38,77 @@ onResize = runFn1 onResizeImpl
 
 main :: Effect Unit
 main = do
-  let col = getColumns
-  let row = getRows
+  let screenCol = getColumns
+  let screenRow = getRows
 
   -- row文ファイルを読み込む
   -- 表示するファイル行数は `rowrange` で制御する
   -- どこかにファイル全体を持ってないと駄目かも
-  a <- for (replicate row "a") \x -> log x
+  -- a <- for (replicate screenRow "a") \x -> log x
 
   onResize showSize
-  onKeypress stdin stdout true getKeyName
+  -- onKeypress stdin stdout true (getKeyName 1)
 
   args <- argv
-  log $ show args
+  -- log $ show args
 
   let fileName = case length args of
                    3 -> getFileName args
                    _ -> Nothing
 
   txt <- readTextFile UTF8 "hoge.txt"
-  let p = Pattern "\n"
+  let txtRecord = createTextRecord txt
   let startRow = 0
-  log $ displayTxt startRow row $ split p txt
-  -- [{row: 1, txt: "あいうえお"}, {row: 2, txt: "かきくけこ"}] 的な配列にする必要があるかも
-  -- filter (\x -> startRow < x.row && x.row < (startRow + row)) txts 的な
-  -- 最後にjoinしましょ
+  -- let displayTxtiiii = filter (\x -> startRow < x.row && x.row < (startRow + screenRow)) txtRecord
+  -- showTxtRecord displayTxtiiii
+  -- log "i"
 
+  onKeypress stdin stdout true (displayTxt screenRow txtRecord)
+
+  -- log $ show txtRecord
+  -- [{row: 1, txt: "あいうえお"}, {row: 2, txt: "かきくけこ"}] 的な配列にする必要があるかも
+  -- 最後にjoinしましょ
+  -- log $ displayTxt startRow row $ split p txt
+
+showTxtRecord :: Array TxtRecord -> Effect Unit
+showTxtRecord t = do
+  a <- for t \x -> do
+     log x.char
+  log ""
+
+type TxtRecord = {row:: Int, char:: String}
+initTxtRecord :: Array TxtRecord
+initTxtRecord = []
+
+createTextRecord :: String -> Array TxtRecord
+createTextRecord txt =  foldl (\x -> \y -> snoc x {row: (length x + 1), char: y}) initTxtRecord (split (Pattern "\n") txt)
 
 getFileName :: Array String -> Maybe String
 getFileName args = head $ reverse args
 
-displayTxt :: Int -> Int -> Array String
-displayTxt startRow row strs = for (replicate row "a") \x -> log x
+displayTxt :: Int -> Array TxtRecord -> PressedKeyInfo -> Effect Unit
+displayTxt screenRow txtReords (PressedKeyInfo pki) = do
+  let startRow = 0
+  clear
+  let name = case pki.name of
+       "h" -> "left"
+       "j" -> "down"
+       "k" -> "up"
+       "l" -> "right"
+       _   -> pki.name
+  let newStartRow = case name of
+       "left"  -> screenRow
+       "down" -> screenRow + 1
+       "up" -> screenRow - 1
+       "right" -> screenRow
+       _   -> screenRow
+  let displayTxt = filter (\x -> newStartRow < x.row && x.row < (newStartRow + screenRow)) txtReords
+  showTxtRecord displayTxt
 
+-- modStartRow :: Int -> State Int Unit
+-- modStartRow i = modify (\sum -> sum)
+-- TODO XXX startRowのいちを保持しておきたいけどやりかたがわからん
+-- stateモナドかreader writer モナドを使えばいい感じにできそうなんだけども
 
 type RowRange = {
    start:: Int
@@ -100,8 +144,8 @@ instance showPressedKeyInfo :: Show PressedKeyInfo where
 }) = "{ sequence: " <> (show s) <> " ,name: " <> n <> " ,ctrl: " <> (show c) <> " ,meta: " <> (show m) <> " ,shift: " <> (show sh) <> " }"
 
 
-getKeyName :: PressedKeyInfo -> Effect Unit
-getKeyName (PressedKeyInfo pki) = do
+getKeyName :: Int -> PressedKeyInfo -> Effect Unit
+getKeyName i (PressedKeyInfo pki) = do
   clear
   let name = case pki.name of
        "h" -> "left"
@@ -110,4 +154,5 @@ getKeyName (PressedKeyInfo pki) = do
        "l" -> "right"
        _   -> pki.name
   log name
+  log $ show i
 
